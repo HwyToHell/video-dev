0fbc13f78c62544929e943dceb4a3f073afe9637
HwyToHell <holger.grzonka@web.de>
2019-09-16 21:33:55 +0200

    groupbox

--- trace-to-db/src/inspectdialog.cpp
+++ trace-to-db/src/inspectdialog.cpp
@@ -1,21 +1,26 @@
 #include "../inc/inspectdialog.h"
 
 #include <QDebug>
+#include <QGroupBox>
 #include <QVBoxLayout>
 
 
 InspectDialog::InspectDialog(QWidget *parent) : QDialog(parent)
 {
     // ToDo vertical
+    QGroupBox *groupRange = new QGroupBox("range", this);
+
     QLabel *frameRange = new QLabel("frame range");
     QLabel *videoOut = new QLabel("image processing");
     QLabel *trackInfo = new QLabel("track info");
     QVBoxLayout *topLayout = new QVBoxLayout();
 
+
     topLayout->addWidget(frameRange);
     topLayout->addWidget(videoOut);
     topLayout->addWidget(trackInfo);
 
-    this->setLayout(topLayout);
+
+    groupRange->setLayout(topLayout);
 }
 
